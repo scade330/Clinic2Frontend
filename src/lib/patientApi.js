828f5f38cfa88ce1970c9a84f69e7c66c245b33c
Zekerie <scade330@gmail.com>
2025-12-01 23:03:06 +0300

    f

--- src/lib/patientApi.js
+++ src/lib/patientApi.js
@@ -1,31 +1,32 @@
 import axios from "axios";
 
-const API_URL = "/api/patients"; // Vite proxy forwards to backend
+// Make sure you use full backend URL in production
+const API_URL = import.meta.env.VITE_BASE_URL + "/api/patients";
 
 // CREATE
 export const createPatient = (patientData) =>
-  axios.post(`${API_URL}/create`, patientData);
+  axios.post(`${API_URL}/create`, patientData, { withCredentials: true });
 
 // READ ALL
 export async function getAllPatients() {
-  const res = await axios.get(`${API_URL}/all`);
+  const res = await axios.get(`${API_URL}/all`, { withCredentials: true });
   return res.data;
 }
 
 // READ ONE
 export async function getPatientById(id) {
-  const res = await axios.get(`${API_URL}/id/${id}`);
+  const res = await axios.get(`${API_URL}/id/${id}`, { withCredentials: true });
   return res.data;
 }
 
 // UPDATE
 export async function updatePatient(id, data) {
-  const res = await axios.put(`${API_URL}/id/${id}`, data);
+  const res = await axios.put(`${API_URL}/id/${id}`, data, { withCredentials: true });
   return res.data;
 }
 
 // DELETE
 export async function deletePatient(id) {
-  const res = await axios.delete(`${API_URL}/id/${id}`);
+  const res = await axios.delete(`${API_URL}/id/${id}`, { withCredentials: true });
   return res.data;
 }
